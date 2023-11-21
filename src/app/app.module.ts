4cf3cf72b599ca685723f634e52303eaf3a6cee9
christianjunior05 <bchrist.junior@gmail.com>
2023-11-21 10:54:22 +0000

    structure du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContainerComponent } from './container/container.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContainerComponent
+    ContainerComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
